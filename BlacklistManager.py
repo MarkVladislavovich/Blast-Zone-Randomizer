b5c02413e60b8957ff605d8dac94c4d2f0a487a7
MarkVladislavovich <pinkerb@bigpond.com>
2025-11-18 23:33:03 +0800

    BlacklistManager.py integration with AssetManager.py

--- BlacklistManager.py
+++ BlacklistManager.py
@@ -1,31 +1,39 @@
 import json
 import os
-import time
+from AssetManager import AssetManager
 
 class BlacklistManager:
-    def __init__(self, file_path="weapons.json"):
-        self.file_path = file_path
+    def __init__(self, asset_manager: AssetManager, file_path="weapons.json"):
+        self.asset_manager = asset_manager
+        self.file_path = asset_manager.resolve(file_path)
         self.weapons = self._load_weapons()  # call internal method to load weapons
 
-        # --- [ INTERNAL ] ---
-        # Loads weapons from the JSON.
+# --- [ INTERNAL ] ---
+    # Loads weapons from the JSON.
     def _load_weapons(self):
         if not os.path.exists(self.file_path):
-            print(f"[ERROR] File missing: {self.file_path}")
-            return []
-        with open(self.file_path, "r") as f:
+            print(f"[ERROR] {self.file_path} is missing, restoring default.")
+            self.weapons = []
+            self._save_weapons()
+            return self.weapons
+
+        with open(self.file_path, "r", encoding='utf-8') as f:
             weapons = json.load(f)
         print(f"Successfully loaded {len(weapons)} from {self.file_path}")
         return weapons
 
         # Saves updates back to the file.
     def _save_weapons(self):
-        with open(self.file_path, "w") as f: # noinspection PyTypeChecker
+        # Saves into the same working directory
+        save_name = os.path.basename(self.file_path)
+        save_path = os.path.join(os.getcwd(), save_name)
+
+        with open(save_path, "w", encoding='utf-8') as f: # noinspection PyTypeChecker
             json.dump(self.weapons, f, indent=4)
 
-        # --- [ EXTERNAL ] ---
-        # Tells a weapon to change its blacklisted state
-    def toggle(self, name):
+# --- [ EXTERNAL ] ---
+    # Tells a weapon to change its blacklisted state
+    def toggle(self, name: str) -> bool | None:
         for w in self.weapons:
             if w["name"].lower() == name.lower():
                 w["blacklisted"] = not w.get("blacklisted", False)
@@ -45,16 +53,7 @@ class BlacklistManager:
             print("\nCurrently blacklisted weapons:")
             for name in blacklisted:
                 print(f"- {name}")
-                time.sleep(0.5)
         return blacklisted
-
-        # [DEBUGGING] Checks if a weapon is blacklisted
-    def is_blacklisted(self, name):
-        for w in self.weapons:
-            if w["name"].lower() == name.lower():
-                return w.get("blacklisted", False)
-        print(f"[ERROR] Weapon '{name}' not found.")
-        return None
 
     # Clears blacklist.... duh
     def clear_blacklist(self):
@@ -68,4 +67,4 @@ class BlacklistManager:
         # Gives back the weapons that are not currently blacklisted
         if full_list:
             return self.weapons
-        return[w for w in self.weapons if not w.get("blacklisted", False)]
+        return [w for w in self.weapons if not w.get("blacklisted", False)]
